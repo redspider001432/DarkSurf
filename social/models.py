a475951580a75871671e09c8ca8ca8263559dbf2
@redspider001432 <vishuserver5@gmail.com>
2022-10-15 17:19:49 +0530

    comit

--- social/models.py
+++ social/models.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images', blank=True)
+    image = models.ImageField(upload_to='post_images')
     write = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
